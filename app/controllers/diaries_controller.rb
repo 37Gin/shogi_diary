a8e7cbf2e88d6d576b0ee78397b8186d45b72bbc
37Gin <yujiterao4425@outlook.jp>
2021-09-16 17:29:37 +0900

    編集機能の実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -20,6 +20,11 @@ class DiariesController < ApplicationController
     @diary = Diary.find(params[:id])
   end
 
+  def update
+    Diary.update(diary_params)
+    redirect_to diary_path
+  end
+
   private
 
   def diary_params
