11b7f9055f2210a12fc42f1257fbbbf0c38d222b
37Gin <yujiterao4425@outlook.jp>
2021-09-21 18:12:21 +0900

    コメントの投稿、表示機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
   def create
+    # binding.pry
+    comment = Comment.create(comment_params)
+    redirect_to "/diaries/#{comment.diary.id}"
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(diary_id: params[:diary_id], user_id: current_user.id)
+  end
+
 end
