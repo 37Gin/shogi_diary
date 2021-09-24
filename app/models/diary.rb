4d23d9fd82d111ba9460b0df220560b67bbc3c9f
37Gin <yujiterao4425@outlook.jp>
2021-09-24 16:54:28 +0900

    アソシエーションの設定

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,6 +2,8 @@ class Diary < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments
+  has_many :tags, through: :diary_tag_relations
+  has_many :diary_tag_relations
 
   validates :title, presence: true
 end
