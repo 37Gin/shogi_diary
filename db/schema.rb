cd1710ff108c97487fe4e03fc52c9cc9bc40f9b8
37Gin <yujiterao4425@outlook.jp>
2021-09-24 16:54:07 +0900

    diary_tagテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_080837) do
+ActiveRecord::Schema.define(version: 2021_09_24_074745) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,21 @@ ActiveRecord::Schema.define(version: 2021_09_21_080837) do
     t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
+  create_table "diary_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "diary_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["diary_id"], name: "index_diary_tag_relations_on_diary_id"
+    t.index ["tag_id"], name: "index_diary_tag_relations_on_tag_id"
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "tag_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -70,4 +85,6 @@ ActiveRecord::Schema.define(version: 2021_09_21_080837) do
   add_foreign_key "comments", "diaries"
   add_foreign_key "comments", "users"
   add_foreign_key "diaries", "users"
+  add_foreign_key "diary_tag_relations", "diaries"
+  add_foreign_key "diary_tag_relations", "tags"
 end
