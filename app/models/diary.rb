a6b36c87acc064e6669f77df3868f16fa59d1bd3
37Gin <yujiterao4425@outlook.jp>
2021-09-28 15:32:44 +0900

    searchメソッドの定義

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -6,4 +6,12 @@ class Diary < ApplicationRecord
   has_many :tags, through: :diary_tag_relations
 
   validates :title, presence: true
+
+  def self.search(search)
+    if search != ""
+      Diary.where(['title LIKE(?) OR content LIKE(?)', "%#{search}%", "%#{search}%"])
+    else
+      Diary.all
+    end
+  end
 end
