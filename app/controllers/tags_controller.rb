99e53d1093b0007385431370e8727ffc3b79fe60
37Gin <yujiterao4425@outlook.jp>
2021-09-29 16:55:12 +0900

    既存のタグを上書きしてしまうエラーを修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,20 +1,18 @@
 class TagsController < ApplicationController
 
   def create
-    tag = Tag.new(tag_params)
-    @tag = Tag.find_by(tag_name: tag.tag_name)
-    if @tag != nil
-      @tag.update(tag_params)
-    else
-      tag.save
+    @tag = DiaryTag.new(tag_params)
+    if @tag.valid?
+      @tag.create
     end
-    redirect_to diary_path(tag.diary_ids[0])
+    redirect_to diary_path(@tag.diary_id)
   end
 
   private
 
   def tag_params
-    params.require(:tag).permit(:tag_name, diary_ids: [])
+    diary_id = params.require(:tag).permit(diary_ids: [])
+    params.require(:tag).permit(:tag_name).merge(diary_id: diary_id["diary_ids"][0])
   end
 
 end
